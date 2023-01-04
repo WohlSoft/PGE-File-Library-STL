049b10ccb3e018fab8ce778c2f316a7ac30da5d5
Wohlstand <admin@wohlnet.ru>
2023-01-04 21:00:34 +0300

    Updated copyright for 2023

--- file_formats.h
+++ file_formats.h
@@ -1,7 +1,7 @@
 /*
  * PGE File Library - a library to process file formats, part of Moondust project
  *
- * Copyright (c) 2014-2022 Vitaly Novichkov <admin@wohlnet.ru>
+ * Copyright (c) 2014-2023 Vitaly Novichkov <admin@wohlnet.ru>
  *
  * The MIT License (MIT)
  *
