5f0a091643ba358226f1f8442b31c748e3210cb0
Wohlstand <admin@wohlnet.ru>
2023-11-14 22:50:02 +0300

    Small fix for WiiU

--- pge_file_lib_globs.cpp
+++ pge_file_lib_globs.cpp
@@ -1398,7 +1398,7 @@ void FileInfo::rebuildData()
 
 #ifdef PGE_FILES_QT
     m_filePath = QFileInfo(m_filePath).absoluteFilePath();
-#elif defined(PGE_NO_REALPATH) || defined(PGE_MIN_PORT) || defined(__3DS__) || defined(VITA) || defined(__SWITCH__)
+#elif defined(PGE_NO_REALPATH) || defined(PGE_MIN_PORT) || defined(__3DS__) || defined(VITA) || defined(__SWITCH__) || defined(__WII__) || defined(__WIIU__)
     // all paths are absolute on minimal target platforms
 #elif !defined(_WIN32)
     char *rez = nullptr;
