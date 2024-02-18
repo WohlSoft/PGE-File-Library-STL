367c3b37baff4be66a83a86bf63388768f7e7190
Wohlstand <admin@wohlnet.ru>
2024-02-18 03:08:42 +0300

    Extended the read tester

--- test/ReadWrite/read_write_test.cpp
+++ test/ReadWrite/read_write_test.cpp
@@ -25,7 +25,9 @@ TEST_CASE("[38A 1.4.0 Level] Load")
 
     bool res = FileFormats::OpenLevelFile(path, data);
 
-    INFO("File: " + path + "; Error: " + data.meta.ERROR_info);
+    INFO("\nFile: " + path + "\n"
+         "Line=" + fromNum(data.meta.ERROR_linenum) + "\n" +
+         "Error: " + data.meta.ERROR_info);
     REQUIRE(res);
     REQUIRE(data.meta.ReadFileValid);
 }
@@ -39,6 +41,146 @@ TEST_CASE("[38A 1.4.0 World] Load")
 
     INFO("\nFile: " + path + "\n"
          "Line=" + fromNum(data.meta.ERROR_linenum) + "\n" +
+         "Error: " + data.meta.ERROR_info);
+    REQUIRE(res);
+    REQUIRE(data.meta.ReadFileValid);
+}
+
+TEST_CASE("[38A 1.4.1 Level] Load")
+{
+    LevelData data;
+    PGESTRING path = TEST_WORKDIR "/test-files/SMBX-38A/test-141.lvl";
+
+    bool res = FileFormats::OpenLevelFile(path, data);
+
+    INFO("\nFile: " + path + "\n"
+         "Line=" + fromNum(data.meta.ERROR_linenum) + "\n" +
+         "Error: " + data.meta.ERROR_info);
+    REQUIRE(res);
+    REQUIRE(data.meta.ReadFileValid);
+}
+
+TEST_CASE("[38A 1.4.1 World] Load")
+{
+    WorldData data;
+    PGESTRING path = TEST_WORKDIR "/test-files/SMBX-38A/test-141.wld";
+
+    bool res = FileFormats::OpenWorldFile(path, data);
+
+    INFO("\nFile: " + path + "\n"
+         "Line=" + fromNum(data.meta.ERROR_linenum) + "\n" +
+         "Error: " + data.meta.ERROR_info);
+    REQUIRE(res);
+    REQUIRE(data.meta.ReadFileValid);
+}
+
+
+TEST_CASE("[38A 1.4.2 Level] Load")
+{
+    LevelData data;
+    PGESTRING path = TEST_WORKDIR "/test-files/SMBX-38A/test-142.lvl";
+
+    bool res = FileFormats::OpenLevelFile(path, data);
+
+    INFO("\nFile: " + path + "\n"
+         "Line=" + fromNum(data.meta.ERROR_linenum) + "\n" +
+         "Error: " + data.meta.ERROR_info);
+    REQUIRE(res);
+    REQUIRE(data.meta.ReadFileValid);
+}
+
+TEST_CASE("[38A 1.4.2 World] Load")
+{
+    WorldData data;
+    PGESTRING path = TEST_WORKDIR "/test-files/SMBX-38A/test-142.wld";
+
+    bool res = FileFormats::OpenWorldFile(path, data);
+
+    INFO("\nFile: " + path + "\n"
+         "Line=" + fromNum(data.meta.ERROR_linenum) + "\n" +
+         "Error: " + data.meta.ERROR_info);
+    REQUIRE(res);
+    REQUIRE(data.meta.ReadFileValid);
+}
+
+
+TEST_CASE("[38A 1.4.3 Level] Load")
+{
+    LevelData data;
+    PGESTRING path = TEST_WORKDIR "/test-files/SMBX-38A/test-143.lvl";
+
+    bool res = FileFormats::OpenLevelFile(path, data);
+
+    INFO("\nFile: " + path + "\n"
+         "Line=" + fromNum(data.meta.ERROR_linenum) + "\n" +
+         "Error: " + data.meta.ERROR_info);
+    REQUIRE(res);
+    REQUIRE(data.meta.ReadFileValid);
+}
+
+TEST_CASE("[38A 1.4.3 World] Load")
+{
+    WorldData data;
+    PGESTRING path = TEST_WORKDIR "/test-files/SMBX-38A/test-143.wld";
+
+    bool res = FileFormats::OpenWorldFile(path, data);
+
+    INFO("\nFile: " + path + "\n"
+         "Line=" + fromNum(data.meta.ERROR_linenum) + "\n" +
+         "Error: " + data.meta.ERROR_info);
+    REQUIRE(res);
+    REQUIRE(data.meta.ReadFileValid);
+}
+
+
+TEST_CASE("[38A 1.4.4 Level] Load")
+{
+    LevelData data;
+    PGESTRING path = TEST_WORKDIR "/test-files/SMBX-38A/test-144.lvl";
+
+    bool res = FileFormats::OpenLevelFile(path, data);
+
+    INFO("File: " + path + "; Error: " + data.meta.ERROR_info);
+    REQUIRE(res);
+    REQUIRE(data.meta.ReadFileValid);
+}
+
+TEST_CASE("[38A 1.4.4 World] Load")
+{
+    WorldData data;
+    PGESTRING path = TEST_WORKDIR "/test-files/SMBX-38A/test-144.wld";
+
+    bool res = FileFormats::OpenWorldFile(path, data);
+
+    INFO("\nFile: " + path + "\n"
+                             "Line=" + fromNum(data.meta.ERROR_linenum) + "\n" +
+         "Error: " + data.meta.ERROR_info);
+    REQUIRE(res);
+    REQUIRE(data.meta.ReadFileValid);
+}
+
+
+TEST_CASE("[38A 1.4.5 Level] Load")
+{
+    LevelData data;
+    PGESTRING path = TEST_WORKDIR "/test-files/SMBX-38A/test-145.lvl";
+
+    bool res = FileFormats::OpenLevelFile(path, data);
+
+    INFO("File: " + path + "; Error: " + data.meta.ERROR_info);
+    REQUIRE(res);
+    REQUIRE(data.meta.ReadFileValid);
+}
+
+TEST_CASE("[38A 1.4.5 World] Load")
+{
+    WorldData data;
+    PGESTRING path = TEST_WORKDIR "/test-files/SMBX-38A/test-145.wld";
+
+    bool res = FileFormats::OpenWorldFile(path, data);
+
+    INFO("\nFile: " + path + "\n"
+                             "Line=" + fromNum(data.meta.ERROR_linenum) + "\n" +
          "Error: " + data.meta.ERROR_info);
     REQUIRE(res);
     REQUIRE(data.meta.ReadFileValid);
