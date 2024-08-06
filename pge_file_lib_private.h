e1af8583a76deafc76edade6416e892f3c0d21a7
Wohlstand <admin@wohlnet.ru>
2024-08-06 18:09:37 +0300

    Fixed build with old Qt
    QString got the .front() and .back() since 5.10.

--- pge_file_lib_private.h
+++ pge_file_lib_private.h
@@ -135,6 +135,28 @@ inline bool      IsEmpty(const PGESTRINGList &str)
 {
     return str.isEmpty();
 }
+#if QT_VERSION >= QT_VERSION_CHECK(5, 10, 0)
+inline QCharRef  Back(PGESTRING &str)
+{
+    return str.back();
+}
+
+inline QChar Back(const PGESTRING &str)
+{
+    return str.back();
+}
+#else
+inline QChar&    Back(PGESTRING &str)
+{
+    return *(str.end() - 1);
+}
+
+inline const QChar&    Back(const PGESTRING &str)
+{
+    return *(str.end() - 1);
+}
+#endif
+
 inline int       toInt(const PGESTRING &str)
 {
     bool okay;
@@ -382,6 +404,17 @@ inline bool IsEmpty(const PGESTRINGList &str)
 {
     return str.empty();
 }
+
+inline char& Back(PGESTRING &str)
+{
+    return str.back();
+}
+
+inline const char& Back(const PGESTRING &str)
+{
+    return str.back();
+}
+
 
 inline int toInt(const PGESTRING &str)
 {
